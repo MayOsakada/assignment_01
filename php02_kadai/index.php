a20f41b707f412e29faa82da9e0684a1e94fdc49
MayOsakada <69710862+MayOsakada@users.noreply.github.com>
2020-10-24 13:06:31 +0900

    Add files via upload

--- php02_kadai/index.php
+++ php02_kadai/index.php
@@ -1,6 +1,5 @@
 
 <?php //テーブル読み込み
-            $alert = null;
             //1.  DB接続します//お約束　ＤＢ名だけつくる
             try {
             //Password:MAMP='root',XAMPP=''
@@ -9,10 +8,14 @@
             exit('DBConnectError:' . $e->getMessage());
             }
             //２．データ取得SQL作成
-            $stmt = $pdo->prepare("SELECT hour, minutes FROM alarm_table");//テーブルにあるデータを持って来るだけなので文字列でいれるとかは気にしなくていいよ
+            $stmt = $pdo->prepare("SELECT hour, minutes, flag FROM alarm_table ORDER BY hour");//テーブルにあるデータを持って来るだけなので文字列でいれるとかは気にしなくていいよ
             $status = $stmt->execute();//成功失敗
             //３．データ表示
             $view="";
+            $houra[]="";
+            $minutesa[]="";
+            $count="";
+            $flag[]="";
             if ($status==false) {//取得失敗
                 //execute（SQL実行時にエラーがある場合）
             $error = $stmt->errorInfo();
@@ -25,9 +28,16 @@
                 $view .= $result['hour']. ':' . $result['minutes'];
                 //.= はテーブル行をどんどん追加していく上書きを防ぐ。’.’がないと一行だけしか表示されない
                 $view.="</p>";
+                $houra[] = $result['hour'];
+                $minutesa[] = $result['minutes'];
+                $flag[] = $result['flag'];
+                $count = $count + 1;
+
             }
             }
+
 ?>
+
 
 <!DOCTYPE html>
 <html>
@@ -36,27 +46,6 @@
 <title>Alarm</title>
 <script src="js/jquery-2.1.3.min.js"></script>
 <link rel="stylesheet" href="css/sample.css">
-</head>
-
-<body>
-<header class="head">
-
-<h1>今日はもういいよ! アラーム</h1>
-<img src="tokei.png">
-<div id="viewTime"></div>
-</header>
-
-
-<div id="check">
-    <input  type="submit" value="今日はもういいですボタン" onClick="todayok()" >
-</div> 
-<div id="outputarea">
-    <!-- 入力データが入る -->
-    <?= $view ?>
-</div>
-<div id="setbutton">
-        <a href="insert.php">時間をセットする</a>
-</div> 
 
 <script>
     //初期表示
@@ -85,99 +74,44 @@
         let p_hours = now.getHours();
         let p_minutes = now.getMinutes();
 
-        <?php //テーブル読み込み
-            //1.  DB接続します//お約束　ＤＢ名だけつくる
-            $hour = date("G");
-            $minutes = date("i");
+        //phpの情報を取得
+        var count = "<?php echo $count; ?>";
+        const hour_array = <?= json_encode($houra) ?>;
+        const minutes_array = <?= json_encode($minutesa) ?>;
+        const flag_array = <?= json_encode($flag) ?>;
 
-            try {
-            //Password:MAMP='root',XAMPP=''
-            $pdo = new PDO('mysql:dbname=gs_bm_table;charset=utf8;host=localhost', 'root', 'root');
-            } catch (PDOException $e) {
-            exit('DBConnectError:' . $e->getMessage());
-            }
-            //２．データ取得SQL作成(フラグに値がないものを取得する)
-            $stmt = $pdo->prepare("SELECT hour, minutes FROM alarm_table WHERE hour = $hour and minutes = $minutes and flag = 'x'");//テーブルにあるデータを持って来るだけなので文字列でいれるとかは気にしなくていいよ
-            $status = $stmt->execute();//成功失敗を格納
-            //３．データ表示
-            if ($status==false) {//取得失敗
-                //execute（SQL実行時にエラーがある場合）
-            $error = $stmt->errorInfo();
-            exit("ErrorQuery:".$error[2]);
-            }else{//取得成功
-            //Selectデータの数だけ自動でループしてくれる
-            //FETCH_ASSOC=http://php.net/manual/ja/pdostatement.fetch.php
-                while($result = $stmt->fetch(PDO::FETCH_ASSOC)){
+        for(let i =0; i<count; i++){
+           if(hour_array[i] = p_hours & minutes_array[i] = p_minutes & flag[i] = null){
+                   alert("時間です");
+           };
 
-
-                    if($result['hour'] = $hour){
-                        if($result['minutes'] = $minutes){
-                            $alert = "alert('時間です') ";
-                        }
-                    }
-                }
-            }
-        ?>
-
-        <?= $alert ?>;
+        };
 
     }
-
-    function todayok(){
-
-
-        //テーブルにフラグを立てる
-
-    }
-
-
-        // //セット クリックイベント
-        // $("#savebutton").on("click",function(){
-        //     console.log(h);
-        //     const hour = $("#hour").val();//keyに入力された値を持ってくる
-        //     const minutes = $("#minutes").val();
-        //     let idcount = $("#outputtable tr").length;
-        //     let settime = hour+':'+minutes;
-        //     localStorage.setItem(idcount,settime);
-            
-        //     const html = '<tr><th id="time'+idcount+'">'+hour+':'+minutes+'</th><td><button id="trash'+idcount+'" onClick="del('+idcount+')">削除</td></tr>';
-        //     $("#outputtable").append(html);
-
-        // });
-
-        
-        // //削除 クリックイベント関数
-        // function del(idcount) {
-
-        //     $('#trash'+idcount).parent().parent().remove();
-            
-        //     //ローカルストレージからも削除
-        //     localStorage.removeItem(idcount);
-        // }
-
-
-
-    // //2.clear クリックイベント
-    // $("#clear").on("click",function(){
-    //     localStorage.clear();
-    //     $("#list").empty();
-    //     alert("ok");
-
-    // });
-
-
-
-    // //3.ページ読み込み：保存データ取得表示 //リロードしても動くよ
-    // for(let i=0; i<localStorage.length; i++){
-    //     const key = localStorage.key(i);
-    //     const memo = localStorage.getItem(key);
-    //     const html = '<tr><td>'+key+'</td><td>'+memo+'</td></tr>';
-    //     $("#list").append(html);
-    // }
-
-
-
 
 </script>
+</head>
+
+<body>
+<header class="head">
+
+<h1>今日はもういいよ! アラーム</h1>
+<img src="tokei.png">
+<div id="viewTime"></div>
+<?= $alert?>
+</header>
+
+
+<div id="check">
+<a href="todayok.php">今日はもういいですボタン</a>
+</div> 
+<div id="outputarea">
+    <!-- 入力データが入る -->
+    <?= $view ?>
+</div>
+<div id="setbutton">
+        <a href="insert.php">時間をセットする</a>
+</div> 
+
 </body>
 </html>
