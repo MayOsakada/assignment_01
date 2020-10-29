39cb5fae8d43bc72bb2e0d5b16e73c27425bb6b6
MayOsakada <69710862+MayOsakada@users.noreply.github.com>
2020-10-30 01:16:50 +0900

    Add files via upload

---
+++ php03_kadai/add.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<form method="POST" action="update.php">
+  <div class="jumbotron">
+   <fieldset>
+    <legend>予定を編集</legend>
+     <label>予定<input type="text" name="yotei1"></label><br>
+     <label>予定<input type="text" name="yotei2"></label><br>
+     <label>予定<input type="text" name="yotei3"></label><br>
+     <input type="submit" value="送信">
+    </fieldset>
+  </div>
+</form>
+    
+</body>
+</html>
